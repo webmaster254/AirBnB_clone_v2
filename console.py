0f1e9a920e33a85097cf2ad68bbb184870873cbe
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2023-03-15 14:02:01 +0300

    Update console.py

--- console.py
+++ console.py
@@ -112,40 +112,70 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
-
-    def do_create(self, line):
-        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        Create a new class instance with given keys/values and print its id.
-        """
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
-
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                kwargs[key] = value
-
-            if kwargs == {}:
-                obj = eval(my_list[0])()
-            else:
-                obj = eval(my_list[0])(**kwargs)
-                storage.new(obj)
-            print(obj.id)
-            obj.save()
-
-        except SyntaxError:
+    
+    def do_create(self, args):
+        """ Create an object of any class"""
+        if not args:
             print("** class name missing **")
-        except NameError:
+            return
+        arg_list = args.split()
+        class_name = arg_list[0]
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
+            return
+
+        new_instance = HBNBCommand.classes[class_name]()
+
+        for arg in arg_list[1:]:
+            param = arg.split('=')
+            key = param[0]
+            val = param[1]
+
+            if val[0] == '\"':
+                val = val.replace('\"', '').replace('_', ' ')
+            elif '.' in val:
+                val = float(val)
+            else:
+                val = int(val)
+
+            setattr(new_instance, key, val)
+
+        new_instance.save()
+        print(new_instance.id)
+
+#     def do_create(self, line):
+#         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+#         Create a new class instance with given keys/values and print its id.
+#         """
+#         try:
+#             if not line:
+#                 raise SyntaxError()
+#             my_list = line.split(" ")
+
+#             kwargs = {}
+#             for i in range(1, len(my_list)):
+#                 key, value = tuple(my_list[i].split("="))
+#                 if value[0] == '"':
+#                     value = value.strip('"').replace("_", " ")
+#                 else:
+#                     try:
+#                         value = eval(value)
+#                     except (SyntaxError, NameError):
+#                         continue
+#                 kwargs[key] = value
+
+#             if kwargs == {}:
+#                 obj = eval(my_list[0])()
+#             else:
+#                 obj = eval(my_list[0])(**kwargs)
+#                 storage.new(obj)
+#             print(obj.id)
+#             obj.save()
+
+#         except SyntaxError:
+#             print("** class name missing **")
+#         except NameError:
+#             print("** class doesn't exist **")
 
     def help_create(self):
         """ Help information for the create method """
